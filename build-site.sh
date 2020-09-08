55d0c24a56a85c62d1a856f5a2711c7d5aba05d3
Julian Elve <julian.elve@gmail.com>
2020-09-08 12:55:03 +0100

    Edit build to pull BB token from environment

--- build-site.sh
+++ build-site.sh
@@ -1,1 +1,2 @@
+sed -i s/\$BB_TOKEN/$BB_TOKEN/ antora-playbook.yml
 DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr NODE_PATH="$(npm -g root)" antora --fetch --generator antora-site-generator-lunr antora-playbook.yml
