d8e5a43a1a577a8d31e04b827c7328cd82fc1573
Julian Elve <julian.elve@gmail.com>
2021-03-10 09:42:10 +0000

    Add script for generating workspace, update README

--- README.md
+++ README.md
@@ -2,4 +2,29 @@
 
 This repository contains an Antora plybook that builds our internal technical documentation.
 
+All instructions assume you are using bash or git-bash shell
+
+## Local build and test
+1. in the root of this repo run `./create-workspace.sh`
+2. in the root of this repo run `npm install`
+3.  `gulp` to create a live preview site
+## Adding a remote document source
+
+### for local editing 
+1. edit `create-workspace.sh`, add relevant line for your remote document source
+2. run `./create-workspace.sh`
+3. edit `local-antora-playbook.yml`
+4. REMEMBER to commit and push the changed `./create-workspace.sh` and `local-antora-playbook.yml`
+5. in the root of this repo run `npm install` follwed by `gulp` to create a live preview site
+6. edit local copy of relevant docs
+7. when done, `cd workspace/<thedocfolder>`
+8. git add, commit and push any changes
+
+
+### for the published build
+
+1. ensure correct permissions on remote repository - the relevant build agent must have access (TODO)
+2. ensure build trigger hooks are set up on the remote (TODO)
+3. edit `antora-playbook.yml`
+4. commit and push
 
---
+++ create-workspace.sh
@@ -0,0 +1,35 @@
+# this script will generate the local workspace
+
+function create_sparse_clone {
+  # args are:
+  # $1 name of directory to create under workspace
+  # $2 remote to clone
+  # $3 git sparse checkout argument
+  # $4 optional branch to checkout
+
+  if [ ! -d ./workspace/$1 ]; then
+   echo "Creating local copy of $1 docs"
+   cd ./workspace
+   git clone --filter=blob:none --no-checkout $2 $1
+   cd $1
+   git sparse-checkout init --cone
+   git sparse-checkout set $3
+   git checkout $4
+   cd ../..
+  else echo "Skipping ./workspace/$1 as exists"
+  fi
+
+}
+
+mkdir -p ./workspace
+
+create_sparse_clone bi  git@github.com:ssatuk/docs.src.bi.git tech
+create_sparse_clone docmetatech git@github.com:ssatuk/docs.src.documentation.git tech
+create_sparse_clone flows  git@bitbucket.org:ssat/ssat.flows.team.git docs/tech
+create_sparse_clone learn  git@github.com:ssatuk/docs.src.learn.git tech
+create_sparse_clone learn-tf   git@github.com:ssatuk/terraform-do-learn.git docs
+create_sparse_clone ma  git@github.com:ssatuk/SSAT.EAI.MarketingAutomation.git doc
+create_sparse_clone processes  git@github.com:ssatuk/docs.src.processes.git tech
+create_sparse_clone ssat.xrm.apps.tm  git@bitbucket.org:ssat/ssat.xrm.apps.tm.git docs/tech
+create_sparse_clone web   https://ssatgitbot:tb4wBa5mZ9ScLcrrzXt6@bitbucket.org/ssat/ssat.web.master.git docs docs
+create_sparse_clone xrm2xero git@github.com:ssatuk/Xrm2Xero.git doc
